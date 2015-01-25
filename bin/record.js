5089e69fd31944f787bf6d39409284e3168081ad
Reto Kaiser <reto@cargomedia.ch>
2015-01-25 16:26:38 +0100

    Add recorder to mongodb

--- bin/record.js
+++ bin/record.js
@@ -1,24 +1,61 @@
 #!/usr/bin/env node
 
 var sha1 = require('sha1');
-var thinkgear = require('../lib/thinkgear/client');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ThinkgearClient = require('../lib/thinkgear/client');
 
-var client = thinkgear.create({
-  appName: 'foo',
-  appKey: sha1('foo'),
-  enableRawOutput: false
+
+MongoClient.connect('mongodb://localhost:27017/neurosky', function (err, db) {
+  assert.equal(null, err);
+  var collection = db.collection('samples');
+
+  var storeSample = function (type, value) {
+    var doc = {time: new Date(), type: type, value: value};
+    collection.insert(doc, function (error, result) {
+      if (error) {
+        console.log('Error storing sample: ' + error);
+      }
+    });
+  };
+
+  var storeSamplesForKeys = function (object) {
+    Object.keys(object).forEach(function (key) {
+      storeSample(key, object[key]);
+    });
+  };
+
+  var thinkgear = ThinkgearClient.create({
+    appName: 'foo',
+    appKey: sha1('foo'),
+    enableRawOutput: false
+  });
+
+  thinkgear.on('data', function (data) {
+    var time = new Date();
+    process.stdout.write("Data: " + time + "\r");
+    if (data.blinkStrength) {
+      storeSample('blinkStrength', data.blinkStrength);
+    }
+    if (data.poorSignalLevel) {
+      storeSample('poorSignalLevel', data.poorSignalLevel);
+    }
+    if (data.eSense) {
+      storeSamplesForKeys(data.eSense);
+    }
+    if (data.eegPower) {
+      storeSamplesForKeys(data.eegPower);
+    }
+  });
+
+  thinkgear.on('error', function (error) {
+    console.log('Error: ' + error);
+  });
+
+  thinkgear.on('close', function () {
+    console.log('Connection closed.');
+  });
+
+  thinkgear.connect();
+
 });
-
-client.on('data', function (data) {
-  console.log(data);
-});
-
-client.on('error', function (error) {
-  console.log(error);
-});
-
-client.on('close', function () {
-  console.log('closing.');
-});
-
-client.connect();

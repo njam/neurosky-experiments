32198d9c24cd3081c5602d94a5840690fb88750e
Reto Kaiser <reto@cargomedia.ch>
2015-02-14 22:55:47 +0100

    Use plotting script for magnitude plot

--- bin/plot.js
+++ bin/plot.js
@@ -2,12 +2,10 @@
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var json2csv = require('json2csv');
-var fs = require('fs');
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var plot = require('../lib/plot').plot;
 
-var htmlPath = __dirname + '/../html/plot/';
+var timeMin = new Date('2015-02-01 00:00:00');
+var timeMax = new Date('2015-02-02 02:00:00');
 
 var typeList = [
   'delta',
@@ -17,11 +15,7 @@ var typeList = [
   'lowBeta',
   'highBeta',
   'lowGamma',
-  'highGamma',
-  'attention',
-  'meditation',
-  'blinkStrength',
-  'poorSignalLevel'
+  'highGamma'
 ];
 
 
@@ -31,34 +25,58 @@ MongoClient.connect('mongodb://localhost:27017/neurosky', function(err, db) {
   console.log('Querying for samples…');
 
   db.collection('samples').aggregate([
-    {$match: {type: {$in: typeList}}},
+    {$match: {type: {$in: typeList}, time: {$gte: timeMin, $lte: timeMax}}},
     {$sort: {time: -1}},
-    {$group: {_id: '$time', valueList: {$push: {type: '$type', value: '$value'}}}}
+    {
+      $group: {
+        _id: '$time',
+        time: {$first: '$time'},
+        valueList: {$push: {type: '$type', value: '$value'}}
+      }
+    }
   ], function(err, dataList) {
     assert.equal(err, null);
 
     console.log('Processing ' + dataList.length + ' samples…');
 
     var sampleList = dataList.map(function(data) {
-      var sample = {time: data._id.toISOString().replace(/T/, ' ')};
+      var sample = {time: data.time};
       typeList.forEach(function(type) {
         sample[type] = undefined;
       });
       data.valueList.forEach(function(value) {
         sample[value['type']] = value['value'];
       });
-      return sample;
-    });
 
-    json2csv({data: sampleList, fields: ['time'].concat(typeList)}, function(err, csv) {
-      assert.equal(err, null);
+      var sampleNormalized = {
+        delta: sample['delta'],
+        theta: sample['theta'],
+        lowAlpha: sample['lowAlpha'],
+        highAlpha: sample['highAlpha'],
+        lowBeta: sample['lowBeta'],
+        highBeta: sample['highBeta'],
+        lowGamma: sample['lowGamma'],
+        highGamma: sample['highGamma']
+      };
 
-      fs.writeFile(htmlPath + 'data.csv', csv, function(err) {
-        assert.equal(err, null);
-
-        console.log('Starting web server at: http://localhost:8080/');
-        connect().use(serveStatic(htmlPath)).listen(8080);
+      var sum = 0;
+      Object.keys(sampleNormalized).forEach(function(key) {
+        sum += sampleNormalized[key];
       });
+      Object.keys(sampleNormalized).forEach(function(key) {
+        sampleNormalized[key] = sampleNormalized[key] / sum;
+      });
+
+      sampleNormalized['time'] = sample.time;
+      if (-1 !== typeList.indexOf('blinkStrength')) {
+        sampleNormalized['blinkStrength'] = sample['blinkStrength'] ? sample['blinkStrength'] / 255 : 0;
+      }
+      if (-1 !== typeList.indexOf('poorSignalLevel')) {
+        sampleNormalized['poorSignalLevel'] = sample['poorSignalLevel'] ? sample['poorSignalLevel'] / 255 : 0;
+      }
+      return sampleNormalized;
     });
+
+    plot(sampleList);
   });
 });
